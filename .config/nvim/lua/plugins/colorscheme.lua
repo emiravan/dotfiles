7ed83ea19ac519d2ad806f8d1073ffd98b0e4673
emiravan <>
2025-07-10 11:39:42 +0300

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,23 @@
 return {
-  "cpea2506/one_monokai.nvim",
-  config = function()
-    require("one_monokai").setup({
-      colors = { bg = "#1E1E1E" },
-    })
-  end,
+  {
+    "f-person/auto-dark-mode.nvim",
+    dependencies = {
+      { "sainnhe/gruvbox-material", priority = 1000, lazy = true },
+    },
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 1
+      require("auto-dark-mode").setup({
+        set_dark_mode = function()
+          vim.api.nvim_set_option_value("background", "dark", {})
+          vim.cmd("colorscheme gruvbox-material")
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option_value("background", "light", {})
+          vim.cmd("colorscheme gruvbox-material")
+        end,
+        update_interval = 10000,
+        fallback = "dark",
+      })
+    end,
+  },
 }
